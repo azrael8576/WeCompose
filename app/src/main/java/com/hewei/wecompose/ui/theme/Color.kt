80e6ab92b7666758bca891d5bee48d9b5c34aa64
Wei.He <wei_he@tk3c.com>
2023-05-29 17:32:27 +0800

    - Style: 新增專案 icon 圖檔 - Style: 新增專案 color 定義

--- app/src/main/java/com/hewei/wecompose/ui/theme/Color.kt
+++ app/src/main/java/com/hewei/wecompose/ui/theme/Color.kt
@@ -1,11 +1,80 @@
 package com.hewei.wecompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.colorspace.ColorSpaces
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val red170 = Color(0xFFFECACA).convert(ColorSpaces.CieXyz)
+val red120 = Color(0xFFFAC6C6).convert(ColorSpaces.CieXyz)
+val red100 = Color(0xFFFA9A9A).convert(ColorSpaces.CieXyz)
+val red90 = Color(0xFFE64646).convert(ColorSpaces.CieXyz)
+val red80 = Color(0xFFD23A3A).convert(ColorSpaces.CieXyz)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val orange170 = Color(0xFFFEC6A1).convert(ColorSpaces.CieXyz)
+val orange120 = Color(0xFFFBD6B1).convert(ColorSpaces.CieXyz)
+val orange100 = Color(0xFFFABC85).convert(ColorSpaces.CieXyz)
+val orange90 = Color(0xFFE59134).convert(ColorSpaces.CieXyz)
+val orange80 = Color(0xFFD2852D).convert(ColorSpaces.CieXyz)
+
+val yellow170 = Color(0xFFFFF599).convert(ColorSpaces.CieXyz)
+val yellow120 = Color(0xFFFFE599).convert(ColorSpaces.CieXyz)
+val yellow100 = Color(0xFFFFD466).convert(ColorSpaces.CieXyz)
+val yellow90 = Color(0xFFE6B100).convert(ColorSpaces.CieXyz)
+val yellow80 = Color(0xFFCC9F00).convert(ColorSpaces.CieXyz)
+
+val green170 = Color(0xFFF3FA99).convert(ColorSpaces.CieXyz)
+val green120 = Color(0xFFD6F099).convert(ColorSpaces.CieXyz)
+val green100 = Color(0xFFB8E366).convert(ColorSpaces.CieXyz)
+val green90 = Color(0xFF82BE00).convert(ColorSpaces.CieXyz)
+val green80 = Color(0xFF74A900).convert(ColorSpaces.CieXyz)
+
+val lightGreen170 = Color(0xFFE2F9C1).convert(ColorSpaces.CieXyz)
+val lightGreen120 = Color(0xFFD9FAC3).convert(ColorSpaces.CieXyz)
+val lightGreen100 = Color(0xFFBFF292).convert(ColorSpaces.CieXyz)
+val lightGreen90 = Color(0xFF86D55E).convert(ColorSpaces.CieXyz)
+val lightGreen80 = Color(0xFF77BE53).convert(ColorSpaces.CieXyz)
+
+val brand170 = Color(0xFFA8ECC1).convert(ColorSpaces.CieXyz)
+val brand120 = Color(0xFF85D9B0).convert(ColorSpaces.CieXyz)
+val brand100 = Color(0xFF4FC687).convert(ColorSpaces.CieXyz)
+val brand90 = Color(0xFF06AD55).convert(ColorSpaces.CieXyz)
+val brand80 = Color(0xFF06994A).convert(ColorSpaces.CieXyz)
+
+val blue170 = Color(0xFFA8DEFF).convert(ColorSpaces.CieXyz)
+val blue120 = Color(0xFF99D9FF).convert(ColorSpaces.CieXyz)
+val blue100 = Color(0xFF66C4FF).convert(ColorSpaces.CieXyz)
+val blue90 = Color(0xFF099BE6).convert(ColorSpaces.CieXyz)
+val blue80 = Color(0xFF0287CC).convert(ColorSpaces.CieXyz)
+
+val indigo170 = Color(0xFFA8CBEC).convert(ColorSpaces.CieXyz)
+val indigo120 = Color(0xFF94B6D9).convert(ColorSpaces.CieXyz)
+val indigo100 = Color(0xFF628FC4).convert(ColorSpaces.CieXyz)
+val indigo90 = Color(0xFF1E68AD).convert(ColorSpaces.CieXyz)
+val indigo80 = Color(0xFF195B99).convert(ColorSpaces.CieXyz)
+
+val purple170 = Color(0xFFB0B5F8).convert(ColorSpaces.CieXyz)
+val purple120 = Color(0xFFB2A7F7).convert(ColorSpaces.CieXyz)
+val purple100 = Color(0xFF8A85F3).convert(ColorSpaces.CieXyz)
+val purple90 = Color(0xFF5A5CE6).convert(ColorSpaces.CieXyz)
+val purple80 = Color(0xFF5151CC).convert(ColorSpaces.CieXyz)
+
+val link170 = Color(0xFFB7B9C9).convert(ColorSpaces.CieXyz)
+val link120 = Color(0xFF99A0B0).convert(ColorSpaces.CieXyz)
+val link100 = Color(0xFF7A849A).convert(ColorSpaces.CieXyz)
+val link90 = Color(0xFF4D6186).convert(ColorSpaces.CieXyz)
+val link80 = Color(0xFF445777).convert(ColorSpaces.CieXyz)
+
+val bwBg100 = Color(0xFFFFFFFF).convert(ColorSpaces.CieXyz)
+val bwBg97 = Color(0xFFF7F7F7).convert(ColorSpaces.CieXyz)
+val bwBg93 = Color(0xFFEDEDED).convert(ColorSpaces.CieXyz)
+val bwBg20 = Color(0xFF333333).convert(ColorSpaces.CieXyz)
+val bwBg0 = Color(0xFF000000).convert(ColorSpaces.CieXyz)
+
+val bw0alpha0_1 = Color(0xFFE6E6E6).convert(ColorSpaces.CieXyz)
+val bw0alpha0_3 = Color(0xFFB3B3B3).convert(ColorSpaces.CieXyz)
+val bw0alpha0_5 = Color(0xFF808080).convert(ColorSpaces.CieXyz)
+val bw0alpha0_9 = Color(0xFF1A1A1A).convert(ColorSpaces.CieXyz)
+
+val bw100Alpha0_1 = Color(0xFF1A1A1A).convert(ColorSpaces.CieXyz)
+val bw100Alpha0_3 = Color(0xFF4D4D4D).convert(ColorSpaces.CieXyz)
+val bw100Alpha0_5 = Color(0xFF808080).convert(ColorSpaces.CieXyz)
+val bw100 = Color(0xFFFFFFFF).convert(ColorSpaces.CieXyz)
